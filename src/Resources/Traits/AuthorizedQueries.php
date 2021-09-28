c54a87ac3cebea154d35abff2db71bd28936debe
Saeed Paracha <muhammad.paracha@pfizer.com>
2021-09-28 14:55:13 +0500

    IndexQuery Trait

---
+++ src/Resources/Traits/AuthorizedQueries.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DigitalCloud\PermissionTool\Resources\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Gate;
+use Spatie\Permission\Contracts\Role;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\Permission\PermissionRegistrar;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Spatie\Permission\Traits\HasPermissions;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+trait AuthorizedQueries
+{
+    /**
+     * Build an "index" query for the given resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        $resource = $request->resource();
+        $permission = sprintf('viewAny-%s', $resource);
+
+        if (Gate::check($permission)) {
+            return $query;
+        }
+        return $query->where(config('permission.column_names.user_id'), request()->user()->id);
+    }
+}
+
