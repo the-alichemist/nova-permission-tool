9dc6e90e49cd73b030c09f777ee5a6cba111ebea
Ali Raza Saleem <thealichemist1@gmail.com>
2022-06-25 05:32:10 +0500

    Vertical display of tools on FormField

--- resources/js/components/fields/permission/FormField.vue
+++ resources/js/components/fields/permission/FormField.vue
@@ -128,11 +128,11 @@
         <div class="ml-14">
           <h2 class="text-xl my-11 ml-2">Tool Permissions</h2>
           <div class="ml-5 mb-7" v-if="tools.length > 0">
-            <div class="flex items-center whitespace-nowrap">
+            <div class="items-center whitespace-nowrap">
               <div
                 v-for="option in tools"
                 :key="option.value"
-                class="flex mb-1 mr-10"
+                class="flex mb-1 mr-10 my-3"
               >
                 <div>
                   <checkbox
