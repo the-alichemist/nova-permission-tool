7f6b0060a7650524bba362e8144bb97bb4c6d0ce
Ali Raza Saleem <thealichemist1@gmail.com>
2022-09-09 20:38:45 +0500

    Role has factories

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -3,9 +3,11 @@
 namespace DigitalCloud\PermissionTool\Models;
 
 use Spatie\Permission\Models\Role as SpatieRole;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends SpatieRole
 {
+    use HasFactory;
     public function setPermissionsAttribute($value)
     {
         $this->syncPermissions($value);
